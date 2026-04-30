032681f2a0a4a63ed0f2b70c3c3bc1951064e55f
Luigi Gutierrez Salazar <110706896+LuigiGutierrez09@users.noreply.github.com>
2026-04-29 20:08:18 -0500

    Update _hash_version.py

--- source/macros/_hash_version.py
+++ source/macros/_hash_version.py
@@ -33,10 +33,10 @@ def main():
         extensiones={".py", ".sas", ".sql", ".txt"}
     )
 
-    output_dir = Path("outputs")
-    output_dir.mkdir(exist_ok=True)
+    #output_dir = Path("outputs")
+    #output_dir.mkdir(exist_ok=True)
 
-    output_file = output_dir / "hash_source.txt"
+    output_file =  "hash_source.txt"
 
     contenido = f"""Hash automático de carpeta source/src
 
